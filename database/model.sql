97f59871713a32fa615fbb7d0bff4a708fa53ad1
Moritz Vieli <m.vieli@ametiqconnect.com>
2019-03-25 07:52:39 +0100

    Made the script rerunnable

--- database/model.sql
+++ database/model.sql
@@ -1,13 +1,14 @@
 -- Create the show designer database model
 
+DROP TABLE IF EXISTS fixture;
 DROP TABLE IF EXISTS manufacturer;
+
 CREATE TABLE manufacturer (
     short_name VARCHAR(200) NOT NULL PRIMARY KEY,
     name VARCHAR(200),
     website VARCHAR(1000)
 );
 
-DROP TABLE IF EXISTS fixture;
 CREATE TABLE fixture (
     name VARCHAR(200) NOT NULL,
     manufacturer_short_name VARCHAR(200) NOT NULL,
